91f427b65e5e1dd270c82009225d99bfa3a79fed
surabhi8 <surabhigupta2442@gmail.com>
2019-06-25 12:01:32 +0530

    configure eslint

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const setupServer = require('./src/server');
 const server = setupServer();
 
 server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at: ${server.info.uri}`);
 });
--- src/server.js
+++ src/server.js
@@ -1,25 +1,23 @@
-'use strict';
+
 
 const Hapi = require('hapi');
 
 const setupServer = () => {
+  const server = new Hapi.Server();
 
-    const server = new Hapi.Server();
+  server.connection({
+    port: 3000,
+    host: 'localhost',
+  });
 
-    server.connection({
-        port: 3000,
-        host: 'localhost'
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: function (request, reply) {
-            reply(`You say ping, I say d!`);
-        }
-    });
-
-    return server;
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler(request, reply) {
+      reply('You say ping, I say d!');
+    },
+  });
+  return server;
 };
 
 module.exports = setupServer;
